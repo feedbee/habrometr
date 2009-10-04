204a0ad7721d9946db9e3c67580c7af1e842a01c
feedbee <feedbee@4373f43e-ebd2-11dd-9b7c-bbba4c9295f9>
2009-10-04 15:59:17 +0000

    version 1.0

--- views/get.php
+++
@@ -1,8 +0,0 @@
-<?php echo $this->menuView(); ?>
-<h1>Код Хаброметров пользователя <a href="./users/<?php print $this->userCode; ?>/"><?php print $this->userCode; ?></a></h1>
-<p>Внимание. Код информера изменять <strong>разрешается</strong>. Например, если вы хотите убрать ссылку на Хаброграф, чтобы информер лаконично вписался в ваш сайт, вы <strong>можете</strong> это сделать.</p>
-<?php $s = $this->sizes; foreach($s as $size) { ?>
-<h2><?php print "{$size['x']}x{$size['y']}"; ?></h2>
-<p><img src="./habrometr_<?php print "{$size['x']}x{$size['y']}"; ?>_<?php print $this->userCode; ?>.png" width="<?php print $size['x']; ?>" height="<?php print $size['y']; ?>" alt="Хаброметр <?php print $this->userCode; ?>" title="Хаброметр <?php print $this->userCode; ?>" /></p>
-<textarea cols="60" rows="6"><a href="<?php print Config::SERVICE_URL; ?>">Хаброметр</a><br /><a href="<?php print Config::SERVICE_URL; ?>users/<?php print $this->userCode; ?>/"><img src="<?php print Config::SERVICE_URL; ?>habrometr_<?php print "{$size['x']}x{$size['y']}"; ?>_<?php print $this->userCode; ?>.png" width="<?php print $size['x']; ?>" height="<?php print $size['y']; ?>" alt="Хаброметр <?php print $this->userCode; ?>" title="Хаброметр <?php print $this->userCode; ?>" /></a></textarea>
-<?php } 
