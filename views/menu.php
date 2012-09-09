013cb3f6879e9e650414a66cd3a1266ca98651ba
Valera Leontyev <feedbee@gmail.com>
2012-09-09 22:04:20 +0300

    Page and Menu templates code formatted

--- views/menu.php
+++ views/menu.php
@@ -3,4 +3,16 @@ if (is_array($this->elements))
 	$elements = $this->elements;
 else
 	$elements = array();
-?><div><?php foreach($elements as $key => $value) { if ($key == $this->active) {?><strong><?php echo $value['text']; ?></strong>&nbsp;&nbsp; <?php } else { ?><a href="<?php echo $value['url']; ?>"<?php if(isset($value['external'])&&$value['external']) echo ' target="blank"'; ?>><?php echo $value['text']; ?></a>&nbsp;&nbsp; <?php }} ?> <div style="float:right">Друзья: <a href="http://www.bankinform.ru/HabraEditor/">ХабраРедактор</a>, <a href="http://habradigest.ru/">habradigest</a>, <a href="http://progg.ru/">progg.ru</a></div></div>
+?>
+<div>
+	<?php foreach($elements as $key => $value) {
+		if ($key == $this->active) {?>
+	<strong><?php echo $value['text']; ?></strong>&nbsp;&nbsp; 
+	<?php } else { ?>
+	<a href="<?php echo $value['url']; ?>"<?php if(isset($value['external'])&&$value['external']) echo ' target="blank"'; ?>><?php echo $value['text']; ?></a>&nbsp;&nbsp; 
+	<?php }} ?> 
+	<div style="float:right">
+		Друзья: <a href="http://www.bankinform.ru/HabraEditor/">ХабраРедактор</a>,
+		<a href="http://habradigest.ru/">habradigest</a>, <a href="http://progg.ru/">progg.ru</a>
+	</div>
+</div>
--- views/page.php
+++ views/page.php
@@ -8,32 +8,41 @@
 	<link rel="stylesheet" href="./stuff/styles.css" media="all" />
 </head>
 <body>
-<?php print $this->body; ?>
-<hr />
-<div style="float:left;">&copy; 2009 Валера Леонтьев a.k.a feedbee. habrometr.ru<br /><span style="font-size:80%;color:#333333">Время считается по UTC+0300 (Московское)</span></div>
-<div style="float:right;">
-<!--LiveInternet counter--><script type="text/javascript"><!--
-document.write("<a href='http://www.liveinternet.ru/click' "+
-"target=_blank><img src='http://counter.yadro.ru/hit?t44.1;r"+
-escape(document.referrer)+((typeof(screen)=="undefined")?"":
-";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
-screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
-";"+Math.random()+
-"' alt='' title='LiveInternet' "+
-"border=0 width=31 height=31><\/a>")//--></script><!--/LiveInternet-->
 
+<?php print $this->body; ?>
+
+<hr />
+
+<div style="float:left;">&copy; 2009—2012 Валера Леонтьев a.k.a feedbee. habrometr.ru<br />
+	<span style="font-size:80%;color:#333333">Время считается по UTC+0300 (Московское)</span></div>
+
+<div style="float:right;">
+	<!--LiveInternet counter-->
+	<script type="text/javascript"><!--
+	document.write("<a href='http://www.liveinternet.ru/click' "+
+	"target=_blank><img src='http://counter.yadro.ru/hit?t44.1;r"+
+	escape(document.referrer)+((typeof(screen)=="undefined")?"":
+	";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+	screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+	";"+Math.random()+
+	"' alt='' title='LiveInternet' "+
+	"border=0 width=31 height=31><\/a>")//--></script>
+	<!--/LiveInternet-->
 </div>
+
 <!-- <?php print $_SERVER['REQUEST_URI']; ?> -->
 
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
 
 <script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-7166216-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+	try {
+	var pageTracker = _gat._getTracker("UA-7166216-1");
+	pageTracker._trackPageview();
+	} catch(err) {}
+</script>
+
 </body>
 </html>
