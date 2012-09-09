fa7d6929cdd1bf6325820ba64fa434cca9aba7b5
Valera Leontyev <feedbee@gmail.com>
2012-09-09 22:05:51 +0300

    Dead links to Progg.ru and link to broken project habradigest removed

--- views/menu.php
+++ views/menu.php
@@ -12,7 +12,6 @@ else
 	<a href="<?php echo $value['url']; ?>"<?php if(isset($value['external'])&&$value['external']) echo ' target="blank"'; ?>><?php echo $value['text']; ?></a>&nbsp;&nbsp; 
 	<?php }} ?> 
 	<div style="float:right">
-		Друзья: <a href="http://www.bankinform.ru/HabraEditor/">ХабраРедактор</a>,
-		<a href="http://habradigest.ru/">habradigest</a>, <a href="http://progg.ru/">progg.ru</a>
+		Друзья: <a href="http://www.bankinform.ru/HabraEditor/">ХабраРедактор</a>
 	</div>
 </div>
